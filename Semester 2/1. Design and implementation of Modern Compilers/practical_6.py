92d06b5c6f3dc512e270670c3feed94f0462aa34
Suraj Mhatre <surajmcodes@gmail.com>
2022-04-05 16:36:01 +0530

    Compiler Pracicals

---
+++ Semester 2/1. Design and implementation of Modern Compilers/practical_6.py
@@ -0,0 +1,23 @@
+'''
+Practical 6
+Q. Write a program code to generate 3-address code.
+'''
+
+if __name__ == "__main__":
+    postfix_expr = input("Enter postfix expression > ").split()
+    operator_set = ('+', '-', '/', '*', '^')
+    stack = []
+    result = str1 = " "
+    count = 0
+    print("Three address code")
+    for i in postfix_expr:
+        if i not in operator_set:
+            stack.append(i)
+            print("Stack:", stack)
+        else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            result = operand1 + i + operand2
+            stack.append("T" + str(count))
+            print("T", count, "=", result)
+            count += 1
